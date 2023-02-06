715af93cd3c16c187c3bd136cc21f173a34b2233
Mattia Caddeo <vincent_28@hotmail.it>
2023-02-06 12:53:04 +0100

    validazione lato server

--- form_in_php/log-in-response.php
+++ form_in_php/log-in-response.php
@@ -2,15 +2,32 @@
 
 <?php
 
-$_GET;
-echo "<pre>";
-echo "GET: ";
-print_r($_GET);
-print_r($_POST);
-echo "</pre>";
+// $_GET;
+// echo "<pre>";
+// echo "GET: ";
+// print_r($_GET);
+// print_r($_POST);
+// echo "</pre>";
 
-echo "La tua email è <br>";
-echo "<strong>".$_POST['email']."</strong>";
+// echo "La tua email è <br>";
+// echo "<strong>".$_POST['email']."</strong>";
+
+/*
+    $ -> varaibile
+    ""/'' -> stringa
+    CICCIO -> costante
+*/
+$test = filter_input(
+    INPUT_GET,
+    'email',
+    FILTER_VALIDATE_EMAIL
+);
+
+if($test === false) {
+    echo "\nLa mail non è valida\n";
+} else {
+    echo "\nGrazie la tua email è valida: $test";
+}
 
 
 ?>
